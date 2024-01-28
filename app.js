3d04f5f0f9cfd1448f48a87f1d2edd6fade8eb73
X <x@Xs-MacBook-Pro.local>
2024-01-29 00:47:13 +0400

    scrollup section

--- app.js
+++ app.js
@@ -22,3 +22,32 @@ const blurHeader = () =>{
 }
 window.addEventListener('scroll',blurHeader)
 
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp. classList.remove('show-scroll')
+}
+
+
+const sectichs = document. querySelectorA11('section[id]')
+
+const scrollActive = () =>{
+    const scrolldown = window. scrollY
+
+    sections. forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current-getAttribute('id'),
+        sectionsClass = document.querySelector('-nav_menu a[href*=' + sectionId + ']')
+
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        sectionsClass.classList.add('active-link')  
+   
+    }else{
+        
+    sectionsClass.classList.remove('active-link')
+    }
+})
+}
+
